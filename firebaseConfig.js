17acff36668b018ec65b816f92e646ac6771c2fa
Gento <a551.return@gmail.com>
2023-05-03 14:26:49 +0900

    不要なfirebase configを削除

--- firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export default {
-  apiKey: "AIzaSyBml4L68HxfrNlWeKKdbjLk9qixiwn0PaM",
-  authDomain: "konkatu-town.firebaseapp.com",
-  projectId: "konkatu-town",
-  storageBucket: "konkatu-town.appspot.com",
-  messagingSenderId: "717955221018",
-  appId: "1:717955221018:web:dfec1145b8f24a094ab3a3",
-  measurementId: "G-1W4J118GLR"
-};
